bb6c1970df409ef8808865c80af87e36f9f3d9ce
yurilima5 <yurilimarosa12@gmail.com>
2025-02-03 10:36:06 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,20 @@
+# print("Lanchonete!")
+
+# salgado = input("Digite seu pedido:")
+
+# bebida = input("bebida:")
+
+# print(f"então você quer {salgado} com {bebida} boa escolha!")
+
+# valor_salgado = int(input('Valor do salgado é:'))
+
+# valor_bebida = int(input('O valor da bebida é:'))
+
+# print(f"O total a pagar é R${valor_bebida + valor_salgado},00 Aproveite!")
+
+idade = int(input("digite sua idade:"))
+
+if idade >= 18:
+    print("você ja pode mandar um pix ao seus instrutores!")
+else:
+    print("desculpe você não pode mandar pix.")
