592e1bc130ac2604124c5c08cd6fb309199110a1
yurilima5 <yurilimarosa12@gmail.com>
2025-02-05 10:30:06 -0300

    loops

---
+++ aula2.py
@@ -0,0 +1,10 @@
+pessoas = 1
+
+while pessoas <= 10:
+    print(f"caramba acabou de chegar o {pessoas}° convidado")
+    pessoas+=1
+
+lista_compras = ["arroz","feijão","batata","pó de café","azeite","Picanha"]
+
+for item in lista_compras:
+    print(item)
